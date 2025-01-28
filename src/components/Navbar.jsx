b7ef04af88bf236c9580c3c8be7af27f695bc8a4
Mesud-Ahmed <mesudahmed94@gmail.com>
2025-01-28 09:09:28 +0300

    feat: add a navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+        <>
+            <div className="shadow-md  bg-white dark:bg-gray-900 dark:text-white duration-200">
+                <div className=" container">
+                        helllo
+                </div>
+
+            </div>
+
+        </>
+    )
+}
