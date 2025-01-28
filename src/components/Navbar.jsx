22178b7374050016c301efe444d50a883598064c
Mesud-Ahmed <mesudahmed94@gmail.com>
2025-01-28 09:28:29 +0300

    feat: add logo and navigation sections with some styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,30 @@
+import  logo  from '../assets/logo.png'
 export default function Navbar() {
     return (
         <>
             <div className="shadow-md  bg-white dark:bg-gray-900 dark:text-white duration-200">
                 <div className=" container">
-                        helllo
+                    <div className='flex justify-between items-center'>
+                        <div>
+                            <a href="#" className='flex items-center gap-2 text-2xl sm:text-3xl font-bold'>
+                                <img src={logo} alt="logo" className='w-10'/>
+                                Al-Diwan
+                            </a>
+                        </div>
+                        <div>
+                            <ul className='flex gap-4'>
+                                <li >
+                                    <a href="#" className='inline-block py-4 px-4 hover:text-primary'>Home</a>
+                                </li>
+                                <li>
+                                    <a href="#" className='inline-block py-4 px-4 hover:text-primary'>About</a>
+                                </li>
+                                <li>
+                                    <a href="#" className='inline-block py-4 px-4 hover:text-primary'>Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
 
             </div>
